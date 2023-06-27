9e8d6de6896860a20e0ad788220bd3e646a99fda
eaespinosa <65977866+eaespinosa@users.noreply.github.com>
2023-06-26 18:13:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-import sqlite3
 from flask import Flask, render_template
 import os
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
